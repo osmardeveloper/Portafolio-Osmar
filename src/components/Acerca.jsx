7ae1097eaebfa25ec73c6fbf0d77a5e54a9c7701
osmyd1990 <osmyd1990@gmail.com>
2023-05-24 22:08:12 -0500

    cv download

--- src/components/Acerca.jsx
+++ src/components/Acerca.jsx
@@ -18,7 +18,7 @@ const Acerca = () => {
            <h3 className='name'><span>Hola, soy</span> <br></br>Osmar Medina</h3>    
            <h2 className="titulo">Desarrollador web <br></br> Full stack</h2>
            <p className='descripcion'> Me encanta la tecnología. Estoy listo para enfrentar los desafíos del mercado laboral, empleando mis conocimientos adquiridos para el desarrollo web.</p>
-        <a href="/hdv-osmar.pdf" download="cv-osma" className='descripcion up'><i className='bx bx-cloud-download'></i> DescargarCV </a>
+        <a href="/hdv-osmar-fullStack.pdf" download="cv-osmar-fullStack" className='descripcion up'><i className='bx bx-cloud-download'></i> DescargarCV </a>
 
         </div>  
        
